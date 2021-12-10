36a3379810018b766f70c172d99bcaa4d53875ce
hermkan <hermann.kanga.dev@gmail.com>
2021-12-09 21:38:11 -0500

    completed

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const tagsElement = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+textarea.focus();
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+    randomSelect();
+  }
+});
 
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
+  tagsElement.innerHTML = '';
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsElement.appendChild(tagEl);
+  });
+  console.log(tags);
+}
+
+function randomSelect() {
+  const times = 30;
+}
